3a5a0697a686ab53fc3db40c642bac28869193a3
George Chang <59218296+gchang12@users.noreply.github.com>
2020-09-16 21:17:38 -1000

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
-#
+#	For use in fixing import statements for user-defined modules within parent module
+
+#	Usual use of module as follows:
+
+#	from fixmy.imports import fix_imports
+#	fix_imports(replace=bool)
+
+#	Replace 'from' import statements with bool = True and just copy the .py files with bool = False
+#	Note: No functionality for 'import [module]' statements; created for personal use, and never imported this way.
